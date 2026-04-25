9fa88eb9d7d81883e197a1bbcafeea98a2043c51
Surya <sprakashr9@gmail.com>
2026-04-25 13:32:00 +0530

    Initial Spring Boot project setup - RuralAgroConnect

---
+++ src/test/java/com/ruralagroconnect/backend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruralagroconnect.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
